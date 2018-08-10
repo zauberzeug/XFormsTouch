ee1918120ce7809cbb95f6aea304148c3b54f3f2
Rodja Trappe <rodja@perpetual-mobile.de>
2018-08-10 16:57:27 +0200

    added sample projects

---
+++ Sample/App.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Sample
+{
+    public class App : Application
+    {
+        public App()
+        {
+            MainPage = new ContentPage()
+            {
+                Content = new Label() { Text = "XFormsTouch", Margin = 40, }
+            };
+        }
+    }
+}
