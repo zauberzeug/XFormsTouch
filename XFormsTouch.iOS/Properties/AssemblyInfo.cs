9ef1358b31ccfe070615c949221bf924d4bde5cd
Rodja Trappe <rodja@perpetual-mobile.de>
2018-08-11 06:53:18 +0200

    Fixed naming in projects

--- XFormsTouch.iOS/Properties/AssemblyInfo.cs
+++ XFormsTouch.iOS/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Xamarin.Forms.TouchEffect.iOS")]
+[assembly: AssemblyTitle("XFormsTouch.iOS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
