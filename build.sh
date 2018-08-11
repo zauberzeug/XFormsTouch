996b97bb539bc777c75668089dfb5e2f2e8a694d
Rodja Trappe <rodja@perpetual-mobile.de>
2018-08-11 16:34:02 +0200

    another try to fix jenkins

--- build.sh
+++ build.sh
@@ -25,6 +25,7 @@ function publishNuGet {
 
 rm -r XFormsTouch.NuGet/bin/Release/
 $NUGET restore XFormsTouch.sln || exit 1
+$NUGET restore XFormsTouch.netstandard2/XFormsTouch.netstandard2.csproj || exit 1
 
 sed -i '' "s/\(<PackageVersion>\).*\(<\/PackageVersion>\)/\1$VERSION\2/" XFormsTouch.NuGet/XFormsTouch.NuGet.nuproj
 msbuild /p:Configuration=Release XFormsTouch.NuGet/XFormsTouch.NuGet.nuproj || exit 1
